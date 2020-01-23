b606d8973177c919d31bf1ef570778ebdeff88bd
Mitch Graves <mitch.graves@nuetex.com>
2020-01-23 01:07:26 -0700

    Module 5 updates
    Module 5 updates

--- jobs/app.py
+++ jobs/app.py
@@ -13,7 +13,7 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql,values=(), commit=False, single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql,values)
     if commit == True:
@@ -34,4 +34,7 @@ def close_connection(exception):
 @app.route('/jobs')
 
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('SELECT job.id, job.title, job.description,
+    job.salary, employer.id as employer_id, employer.name as employer_name
+    FROM job JOIN employer ON employer.id = job.employer.id = job.employer_id')
+    return render_template('index.html, jobs=jobs')
