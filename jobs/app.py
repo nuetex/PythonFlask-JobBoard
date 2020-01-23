2d4d0326af7c3521cd7e0fab294c31d4bded9fb8
Mitch Graves <mitch.graves@nuetex.com>
2020-01-23 00:11:19 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,20 +1,19 @@
+
 import sqlite3
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
-app = Flask(__name__)
 
-@app.route('/')
-@app.route('/jobs')
+app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
-    return _connection
+    return connection
 
-def execute_sql(sql,values=(),commit=False, single=False):
+def execute_sql(sql,values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql,values)
     if commit == True:
@@ -27,10 +26,12 @@ def execute_sql(sql,values=(),commit=False, single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-    connection = getattr(g, 'connection', None)
+    connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
+@app.route('/')
+@app.route('/jobs')
 
 def jobs():
     return render_template('index.html')
