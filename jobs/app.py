312675b86b871253a9676ec28bab0e3572c1eab4
Mitch Graves <mitch.graves@nuetex.com>
2020-01-22 23:58:26 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -10,8 +10,9 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        connection = g._connection = sqlite.connect(PATH)
-        return _connection
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return _connection
 
 def execute_sql(sql,values=(),commit=False, single=False):
     connection = open_connection()
